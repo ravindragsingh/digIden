1ea42e7b142ef61ef84cf450608b4dd3f9aa036d
ravindragsingh <arien.641@gmail.com>
2019-06-29 00:44:14 -0400

    add images

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterNowComponent } from './register-now/register-now.component';
+import { StartPageComponent } from './start-page/start-page.component';
 
 
 const routes: Routes = [
-{path:'', component:LoginComponent},
+{path:'', component:StartPageComponent},
+{path:'login', component:LoginComponent},
 {path:'register', component:RegisterNowComponent}
 ];
 
